f9ee7e7431121ef680b4895d662c6d880246b5d0
anvarovna9229 <130303368+anvarovna9229@users.noreply.github.com>
2023-05-31 18:59:57 +0600

    Create script.js

---
+++ assignment 4(1)/js/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+    function checkName() {
+      const nameId = $('#nameId');
+      const surnameId = $('#surnameId');
+  
+      for (let lett of nameId.val()) {
+        if (!isNaN(lett)) {
+          alert('Numbers in the name field are not allowed');
+          return false;
+        }
+      }
+  
+      for (let lett of surnameId.val()) {
+        if (!isNaN(lett)) {
+          alert('Numbers in the surname field are not allowed');
+          return false;
+        }
+      }
+  
+      return true;
+    }
+  
+    const emailInput = $('#emailInput');
+  
+    function checkEmail() {
+      if (emailInput.val().includes('@') && emailInput.val().includes('.')) {
+        return true;
+      } else {
+        emailInput.val('');
+        alert('Wrong mail form');
+        return false;
+      }
+    }
+  
+    function checkPhone() {
+      const phoneInp = $('#phoneInp').val();
+      if (phoneInp[0] == '+' && phoneInp.length == 12) {
+        return true;
+      } else {
+        alert('Incorrect number form');
+        return false;
+      }
+    }
+  
+    const birthInp = $('#birthInp');
+    birthInp.on('change', function checkDate() {
+      if (+birthInp.val().split('-')[0] > new Date().getFullYear() - 122) {
+        return true;
+      } else {
+        alert('You entered the wrong year');
+        birthInp.val('');
+        return false;
+      }
+    });
+  
+    const submitBtn = $('#submitBtn');
+    submitBtn.on('click', function() {
+      if (checkName() && checkPhone() && checkEmail()) {
+        alert('You have successfully registered!');
+      } else {
+        alert('Fix the errors and resend');
+      }
+    });
+  });
+  
+  $('#submitBtn').css('background-color', '#cf7e21');
